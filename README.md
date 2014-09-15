b965b82aacc4002c07e4300b947720b66b371fd4
Tony Duan <tony@tonyduan.com>
2014-09-14 17:45:36 -0700

    Project layout.

--- README.md
+++
@@ -1,4 +0,0 @@
-clj-yahoo-boss
-==============
-
-A client for the Yahoo BOSS Search API
---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject clj-yahoo-boss "0.0.1"
+
+  :description "A Clojure client for the Yahoo BOSS Search API"
+  :url "https://github.com/tonyduan/clj-yahoo-boss"
+
+  :license {
+    :name "MIT License"
+    :url "http://opensource.org/licenses/MIT"}
+
+  :dependencies [
+    [org.clojure/clojure "1.6.0"]])
